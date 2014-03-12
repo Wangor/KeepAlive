100db4d3e81f7b241af81cdc682a177802f9f44d
Wangor <matt@matt-b.ch>
2014-03-12 19:57:22 +0100

    Update Program.cs

--- KeepAlive/Program.cs
+++ KeepAlive/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             new KeepAlive("http://www.equestriano.com", 15).Start();
+            new KeepAlive("http://www.handworkatelier.ch", 15).Start();
+            new KeepAlive("http://www.vetkp3.ch", 15).Start();
         }
     }
 }
