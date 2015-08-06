b7ff69fbad948934bf71c90c2032bb9d7977e57e
Matthias Bärtschi <matt@matt-b.ch>
2015-08-06 07:41:10 +0200

    Update Program.cs

--- KeepAlive/Program.cs
+++ KeepAlive/Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            new KeepAlive("http://www.equestriano.com", 15).Start();
-            new KeepAlive("http://www.handworkatelier.ch", 15).Start();
-            new KeepAlive("http://www.vetkp3.ch", 15).Start();
+            new KeepAlive("http://http://coursebooking.apphb.com/", 15).Start();
         }
     }
 }
