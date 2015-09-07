5aa67577021bc39139b1ff808ed01fe4b3945be4
Matthias Bärtschi <matt@matt-b.ch>
2015-09-08 00:41:09 +0200

    Update Program.cs

--- KeepAlive/Program.cs
+++ KeepAlive/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             new KeepAlive("http://coursebooking.apphb.com/", 15).Start();
+            new KeepAlive("http://coursebooking.matt-b.ch/", 15).Start();
         }
     }
 }
